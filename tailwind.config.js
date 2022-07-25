82f577c938a5efa022fdb60f461f604fe345bb45
majidzahedi <eemajidzahedi@hotmail.com>
2022-07-25 17:36:46 +0430

    feat: configured tailwind according to style-guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        sans:['Rubik', 'sans-serif']
+      },
       colors: {
-        lightGreen: "#56B280",
-        black: "#272727",
+        // Primary
+        moderateBlue: 'hsl(238, 40%, 52%)',
+        softRed: 'hsl(358, 79%, 66%)',
+        lightGrayishBlue: 'hsl(239, 57%, 85%)',
+        paleRed: 'hsl(357, 100%, 86%)',
+        // Neutral
+        darkBlue:'hsl(212, 24%, 26%)',
+        grayishBlue:'hsl(211, 10%, 45%)',
+        lightGray:'hsl(223, 19%, 93%)',
+        veryLightGray:'hsl(228, 33%, 97%)',
       },
     },
   },
