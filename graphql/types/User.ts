862ab74ecca763f7771ef6b016e974e188dd5eb0
majidzahedi <eemajidzahedi@hotmail.com>
2022-07-25 20:30:30 +0430

    feat: added user resolver

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -9,13 +9,28 @@ export const User = objectType({
   },
 });
 
-export const userQuery = extendType({
+export const usersQuery = extendType({
   type: "Query",
   definition(t) {
     t.field("users", {
       type: list("User"),
       async resolve(_, __, context) {
         const users = await context.prisma.user.findMany();
+        return users;
+      },
+    });
+  },
+});
+
+export const userQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.field("user", {
+      type: "User",
+      async resolve(_, __, context) {
+        const users = await context.prisma.user.findUnique({
+          where: { id: context.userId },
+        });
         return users;
       },
     });
