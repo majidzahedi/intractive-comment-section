e0e21904d65dfa446d33709822fb88c746acce7e
majidzahedi <eemajidzahedi@hotmail.com>
2022-07-26 12:38:29 +0430

    fix: comments width problem

--- components/CommnetsList.jsx
+++ components/CommnetsList.jsx
@@ -2,7 +2,7 @@ import Comment from "./Commnet";
 
 export default function CommnetsList({ comments, user }) {
   return (
-    <div className="flex flex-col space-y-1 py-2 ">
+    <div className="flex flex-col space-y-1 py-2 w-full">
       {comments &&
         comments.map((comment) => (
           <Comment key={comment.id} comment={comment} user={user} />
