0c62410aa96000e5f23469f75f55b86a558bc731
majidzahedi <eemajidzahedi@hotmail.com>
2022-07-26 13:44:34 +0430

    refactor: comment component styled and refactored

--- components/Commnet.jsx
+++ components/Commnet.jsx
@@ -24,70 +24,34 @@ export default function Comment({ comment, user }) {
 
   return (
     <>
-      <div className="flex w-full p-4 space-x-4 items-start bg-white rounded-lg">
-        <div className="flex flex-col rounded-lg p-4 items-center space-y-1 bg-veryLightGray">
-          <span className="text-grayishBlue font-bold">+</span>
-          <span className="text-moderateBlue font-bold">4</span>
-          <span className="text-grayishBlue font-bold">-</span>
+      <div className="flex flex-col-reverse w-full p-4 space-y-0 items-start bg-white rounded-lg md:flex-row md:space-x-4">
+        <div className="flex justify-between w-full md:w-auto mt-2 md:mt-0">
+          <Rate />
+          <Crud
+            visability="flex md:hidden"
+            isEditing={isEditing}
+            user={user}
+            comment={comment}
+            setIsEditing={setIsEditing}
+            isReply={isReply}
+            setIsReply={setIsReply}
+            handleDelete={handleDelete}
+          />
         </div>
         <div className="flex flex-col w-full space-y-4">
-          <div className="flex justify-between items-center">
-            <div className="flex space-x-3 items-center">
-              <img
-                src={`/images/avatars/image-${comment.user.name}.png`}
-                alt=""
-                className="w-7"
-              />
-              <p className="font-medium text-darkBlue text-sm">
-                {comment.user.name}
-              </p>
-              <p
-                hidden={comment.user.name !== user.name}
-                className="text-white bg-moderateBlue px-1 text-xs font-medium rounded-sm"
-              >
-                you
-              </p>
-              <p className="text-lightGrayishBlue text-sm">1 Week ago</p>
-            </div>
-            <div className="flex space-x-5">
-              <button
-                className={`flex items-center space-x-1 hover:opacity-70 text-softRed font-medium ${
-                  comment.user.name !== user.name && "hidden"
-                }`}
-                onClick={handleDelete}
-              >
-                <img src="/images/icon-delete.svg" alt="" />
-                <p>Delete</p>
-              </button>
-              <button
-                className={`flex items-center space-x-1 hover:opacity-70 text-moderateBlue font-medium ${
-                  comment.user.name !== user.name && "hidden"
-                }`}
-                onClick={() => setIsEditing(!isEditing)}
-              >
-                <img src="/images/icon-edit.svg" alt="" />
-                <span>Edit</span>
-              </button>
-              <button
-                className={`flex items-center space-x-1 hover:opacity-70 text-moderateBlue font-medium ${
-                  comment.user.name === user.name && "hidden"
-                }`}
-                onClick={() => setIsReply(!isReply)}
-              >
-                <img src="/images/icon-reply.svg" alt="" />
-                <span>Reply</span>
-              </button>
-            </div>
-          </div>
-          {isEditing ? (
-            <FormInput
-              isEditing={isEditing}
-              comment={comment}
-              setIsEditing={setIsEditing}
-            />
-          ) : (
-            <p className="text-base text-grayishBlue">{comment.comment}</p>
-          )}
+          <UserSummery
+            comment={comment}
+            user={user}
+            setIsReply={setIsReply}
+            setIsEditing={setIsEditing}
+            isEditing={isEditing}
+            isReply={isReply}
+          />
+          <CommentField
+            setIsEditing={setIsEditing}
+            comment={comment}
+            isEditing={isEditing}
+          />
         </div>
       </div>
       {isReply && (
@@ -98,15 +62,132 @@ export default function Comment({ comment, user }) {
           setIsReply={setIsReply}
         />
       )}
-      <div className="flex ">
-        <div className="w-1  mx-4 md:mx-12  bg-grayishBlue"></div>
-        <div className="flex space-y-2 flex-col w-full ">
-          {comment.replies &&
-            comment.replies.map((reply) => (
-              <Comment key={reply.id} comment={reply} user={user} />
-            ))}
-        </div>
-      </div>
+      <Replies comment={comment} user={user} />
     </>
   );
 }
+
+const UserSummery = ({
+  comment,
+  user,
+  setIsEditing,
+  setIsReply,
+  isReply,
+  isEditing,
+}) => {
+  const [deleteComment] = useMutation(DELETE);
+
+  function handleDelete() {
+    deleteComment({
+      variables: { commentId: comment.id },
+      refetchQueries: ["comments"],
+    });
+  }
+
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex space-x-3 items-center">
+        <img
+          src={`/images/avatars/image-${comment.user.name}.png`}
+          alt=""
+          className="w-7"
+        />
+        <p className="font-medium text-darkBlue text-sm">{comment.user.name}</p>
+        <p
+          hidden={comment.user.name !== user.name}
+          className="text-white bg-moderateBlue px-1 text-xs font-medium rounded-sm"
+        >
+          you
+        </p>
+        <p className="text-lightGrayishBlue text-sm">1 Week ago</p>
+      </div>
+      <Crud
+        isEditing={isEditing}
+        user={user}
+        comment={comment}
+        setIsEditing={setIsEditing}
+        isReply={isReply}
+        setIsReply={setIsReply}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
+};
+
+const Crud = ({
+  isEditing,
+  user,
+  comment,
+  setIsEditing,
+  isReply,
+  setIsReply,
+  handleDelete,
+  visability = "md:flex hidden",
+}) => (
+  <div className={` space-x-5 ${visability}`}>
+    <button
+      className={`flex items-center space-x-1 hover:opacity-70 text-softRed font-medium ${
+        comment.user.name !== user.name && "hidden"
+      }`}
+      onClick={handleDelete}
+    >
+      <img src="/images/icon-delete.svg" alt="" />
+      <p>Delete</p>
+    </button>
+    <button
+      className={`flex items-center space-x-1 hover:opacity-70 text-moderateBlue font-medium ${
+        comment.user.name !== user.name && "hidden"
+      }`}
+      onClick={() => setIsEditing((prev) => !prev)}
+    >
+      <img src="/images/icon-edit.svg" alt="" />
+      <span>{isEditing ? "CANCEL" : "EDIT"}</span>
+    </button>
+    <button
+      disabled={user.name === "Anonymous"}
+      className={`flex items-center space-x-1 hover:opacity-70 text-moderateBlue font-medium disabled:opacity-70 ${
+        comment.user.name === user.name && "hidden"
+      }`}
+      onClick={() => setIsReply((prev) => !prev)}
+    >
+      <img src="/images/icon-reply.svg" alt="" />
+      <span>{isReply ? "CANCEL" : "REPLY"}</span>
+    </button>
+  </div>
+);
+
+const Rate = () => {
+  return (
+    <div className="flex rounded-lg py-2 px-4 md:p-4 space-x-3 items-center justify-center  bg-veryLightGray md:flex-col md:space-y-1 md:space-x-0">
+      <button className="text-grayishBlue font-bold ">+</button>
+      <button className="text-moderateBlue font-bold">4</button>
+      <button className="text-grayishBlue font-bold">-</button>
+    </div>
+  );
+};
+
+const Replies = ({ comment, user }) => {
+  return (
+    <div className="flex ">
+      <div className="w-1  mx-4 md:mx-12  bg-grayishBlue"></div>
+      <div className="flex space-y-2 flex-col w-full ">
+        {comment.replies &&
+          comment.replies.map((reply) => (
+            <Comment key={reply.id} comment={reply} user={user} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+const CommentField = ({ isEditing, comment, setIsEditing }) => {
+  return isEditing ? (
+    <FormInput
+      isEditing={isEditing}
+      comment={comment}
+      setIsEditing={setIsEditing}
+    />
+  ) : (
+    <p className="text-base text-grayishBlue text-justify">{comment.comment}</p>
+  );
+};
