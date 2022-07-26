8c125c9715dd8d1a3122fd5dc738ceb8e47b739d
majidzahedi <eemajidzahedi@hotmail.com>
2022-07-26 14:38:22 +0430

    pkg: added prettier and tailwind-scrollbar-hide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{
-        sans:['Rubik', 'sans-serif']
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
       },
       colors: {
         // Primary
-        moderateBlue: 'hsl(238, 40%, 52%)',
-        softRed: 'hsl(358, 79%, 66%)',
-        lightGrayishBlue: 'hsl(239, 57%, 85%)',
-        paleRed: 'hsl(357, 100%, 86%)',
+        moderateBlue: "hsl(238, 40%, 52%)",
+        softRed: "hsl(358, 79%, 66%)",
+        lightGrayishBlue: "hsl(239, 57%, 85%)",
+        paleRed: "hsl(357, 100%, 86%)",
         // Neutral
-        darkBlue:'hsl(212, 24%, 26%)',
-        grayishBlue:'hsl(211, 10%, 45%)',
-        lightGray:'hsl(223, 19%, 93%)',
-        veryLightGray:'hsl(228, 33%, 97%)',
+        darkBlue: "hsl(212, 24%, 26%)",
+        grayishBlue: "hsl(211, 10%, 45%)",
+        lightGray: "hsl(223, 19%, 93%)",
+        veryLightGray: "hsl(228, 33%, 97%)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
