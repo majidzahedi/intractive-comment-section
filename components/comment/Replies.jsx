df8b474aab897520223adbd2b523353134b08c3a
majidzahedi <eemajidzahedi@hotmail.com>
2022-08-06 18:37:12 +0430

    fix: style adjustment

--- components/comment/Replies.jsx
+++ components/comment/Replies.jsx
@@ -2,9 +2,9 @@ import Comment from ".";
 
 export const Replies = ({ comment, user }) => {
   return (
-    <div className="flex ">
+    <div className="flex pt-1">
       <div className="mx-4  w-1 bg-grayishBlue  md:mx-12"></div>
-      <div className="flex w-full flex-col space-y-2 ">
+      <div className="flex w-full flex-col space-y-1 ">
         {comment.replies &&
           comment.replies.map((reply) => (
             <Comment key={reply.id} comment={reply} user={user} />
