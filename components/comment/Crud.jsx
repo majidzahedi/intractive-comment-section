d2cd74aaff224091655552ac07adf1d7ac9726a2
majidzahedi <eemajidzahedi@hotmail.com>
2022-08-10 19:35:41 +0430

    refactor: crud component

--- components/comment/Crud.jsx
+++ components/comment/Crud.jsx
@@ -1,7 +1,9 @@
+import { clsx } from "clsx";
+
+import { useStore } from "../../utils/index";
 import DeleteIcon from "../../public/images/icon-delete.svg";
 import EditIcon from "../../public/images/icon-edit.svg";
 import ReplyIcon from "../../public/images/icon-reply.svg";
-import { useStore } from "../../utils/index";
 
 export const Crud = ({
   commentId,
@@ -12,45 +14,45 @@ export const Crud = ({
   toggleReply,
   visability = "hidden md:flex",
 }) => {
-  const { openModal, isOpen } = useStore();
+  const { openModal, modal } = useStore();
 
   return (
-    <div className={`${visability} space-x-5 rtl:space-x-reverse  `}>
+    <div className={`${visability} space-x-5 rtl:space-x-reverse`}>
       <button
-        className={`text-softRed flex items-center space-x-1 font-medium hover:opacity-70 disabled:opacity-90 rtl:flex-row-reverse  ${
-          !isCommentOwner && "hidden"
-        } ${isOpen ? "animate-pulse" : ""}`}
+        className={clsx(
+          "flex items-center space-x-1 font-medium text-latteMarron hover:opacity-70 disabled:opacity-50 rtl:flex-row-reverse dark:text-mochaMarron",
+          isCommentOwner ? "block" : "hidden"
+        )}
         onClick={() => openModal(commentId)}
-        disabled={isOpen}
+        disabled={modal.isOpen}
       >
-        <DeleteIcon className="text-latteMarron dark:text-mochaMarron" />
-        <p className="text-latteMarron dark:text-mochaMarron">حذف</p>
+        <DeleteIcon />
+        <p>حذف</p>
       </button>
+
       <button
-        className={`text-moderateBlue flex items-center space-x-1 font-medium hover:opacity-70  rtl:flex-row-reverse  ${
-          !isCommentOwner && "hidden"
-        }`}
+        className={clsx(
+          " flex items-center space-x-1 font-medium text-latteSapphire hover:opacity-50 rtl:flex-row-reverse  dark:text-mochaSapphire",
+          isCommentOwner ? "block" : "hidden"
+        )}
         onClick={toggleEdit}
       >
-        <EditIcon className="text-latteSapphire dark:text-mochaSapphire" />
-        <span className="text-latteSapphire dark:text-mochaSapphire">
-          {settings.isEditing ? "لغو" : "ویرایش"}
-        </span>
+        <EditIcon />
+        <p>{settings.isEditing ? "لغو" : "ویرایش"}</p>
       </button>
+
       <button
         disabled={isAnonymous}
-        className={`text-moderateBlue flex items-center space-x-1 font-medium hover:opacity-70 disabled:opacity-70 rtl:flex-row-reverse  ${
-          isCommentOwner && "hidden"
-        }`}
+        className={clsx(
+          "flex items-center space-x-1 font-medium text-latteSapphire hover:opacity-70 disabled:opacity-70 rtl:flex-row-reverse dark:text-mochaSapphire",
+          isCommentOwner ? "hidden" : "block"
+        )}
         onClick={() => {
-          console.log("reply clicked");
           toggleReply();
         }}
       >
-        <ReplyIcon className="text-latteSapphire dark:text-mochaSapphire" />
-        <span className="text-latteSapphire dark:text-mochaSapphire">
-          {settings.isReply ? "لغو" : "پاسخ"}
-        </span>
+        <ReplyIcon />
+        <p>{settings.isReply ? "لغو" : "پاسخ"}</p>
       </button>
     </div>
   );
